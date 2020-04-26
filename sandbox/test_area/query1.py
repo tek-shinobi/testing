66a3750dc8022f35f67bd94315e8b78d252cdb2c
tek-shinobi <tek.shinobi.ninja@gmail.com>
2020-04-26 17:43:17 +0400

    queries for displaying columns and order_details

--- sandbox/test_area/query1.py
+++ sandbox/test_area/query1.py
@@ -3,10 +3,14 @@ import pprint
 from sqlalchemy import inspect
 from core_lib.meta import engine
 
-inspector = inspect(engine)
-pp = pprint.PrettyPrinter(width=41, compact=True)
-print("TABLE NAMES")
-pp.pprint(inspector.get_table_names())
-print("COLUMN NAMES for products")
-for column in inspector.get_columns("products"):
-    pp.pprint(column)
+if __name__ == "__main__":
+    inspector = inspect(engine)
+    pp = pprint.PrettyPrinter(width=41, compact=True)
+    print("TABLE NAMES")
+    pp.pprint(inspector.get_table_names())
+    print("COLUMN NAMES for products")
+    for column in inspector.get_columns("products"):
+        pp.pprint(column)
+    print("COLUMN NAMES for order_details")
+    for column in inspector.get_columns("order_details"):
+        pp.pprint(column)
---
+++ sandbox/test_area/query2.py
@@ -0,0 +1,30 @@
+"""
+1. Order Subtotals
+For each order, calculate a subtotal for each Order (identified by OrderID).
+This is a simple query using GROUP BY to aggregate data for each order.
+
+SELECT "OrderID", round(SUM("UnitPrice"*"Quantity"*(1-"Discount"))::Numeric,2) as total
+	FROM public.order_details as od
+	GROUP BY "OrderID"
+	ORDER BY "OrderID";
+"""
+
+from sqlalchemy.sql.expression import literal, func
+from sqlalchemy import cast, Numeric
+from core_lib import meta
+from core_lib import utils
+
+from test_area import model
+
+od = model.order_details
+summation = func.sum(
+    od.columns.UnitPrice * od.columns.Quantity * (1 - od.columns.Discount)
+)
+rounding = func.round(cast(summation, Numeric(10, 2)), 2)
+query = (
+    meta.session.query(od.columns.OrderID, rounding.label("total"))
+    .group_by(literal(1))
+    .order_by(literal(1))
+    .limit(10)
+)
+utils.print_table(query)
