e0481a7946794955b2485ab56609a9eddbad4bfd
tek-shinobi <tek.shinobi.ninja@gmail.com>
2020-04-29 15:47:55 +0400

    added postgres query

--- sandbox/test_area/query4.py
+++ sandbox/test_area/query4.py
@@ -3,6 +3,14 @@ Build a query to show the Orders list for each employee, showing destination
 Country, EmployeeID. Show two new fields in the final result set. One of
 them to calculate the sum of Freight sold by every employee, and the other to
 calculate the sum of Freight sold by every Employee in every Country.
+SELECT
+	o."ShipCountry", o."EmployeeID",
+	sum(o."Freight") over(partition by o."EmployeeID") employeeid_freight,
+	sum(o."Freight") over(partition by o."EmployeeID", o."ShipCountry") employeeid_freight_country
+FROM
+	public.orders o
+ORDER BY
+	o."EmployeeID", o."ShipCountry"
 """
 
 from sqlalchemy.sql.expression import literal, func
@@ -19,16 +27,21 @@ rounding = func.round(cast(summation, Numeric(10, 2)), 2)
 subtotal_cte = (
     meta.session.query(od.c.OrderID, rounding.label("total")).group_by(literal(1))
 ).cte("subtotal")
-query = meta.session.query(
-    orders.c.ShipCountry,
-    orders.c.EmployeeID,
-    func.sum(orders.c.Freight)
-    .over(partition_by=orders.c.EmployeeID)
-    .label("freight_by_employee"),
-    func.sum(orders.c.Freight)
-    .over(partition_by=[orders.c.EmployeeID, orders.c.ShipCountry])
-    .label("freight_by_employee_country"),
-).limit(20)
+query = (
+    meta.session.query(
+        orders.c.ShipCountry,
+        orders.c.EmployeeID,
+        func.sum(orders.c.Freight)
+        .over(partition_by=orders.c.EmployeeID)
+        .label("freight_by_employee"),
+        func.sum(orders.c.Freight)
+        .over(partition_by=[orders.c.EmployeeID, orders.c.ShipCountry])
+        .label("freight_by_employee_country"),
+    )
+    .order_by(orders.c.EmployeeID, orders.c.ShipCountry)
+    .limit(20)
+)
+# ).order_by([orders.c.EmployeeID, orders.c.ShipCountry]).limit(20)
 # query = (
 #    meta.session.query(
 #        orders.c.ShippedDate,
